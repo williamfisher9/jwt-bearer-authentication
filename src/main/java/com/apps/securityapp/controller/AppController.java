1d76ad2ac23a0137a2f67028ff08f24eae2518c8
williamfisher9 <fisher.a.william@gmail.com>
2023-12-27 04:27:46 -0600

    added signup endpoint to the controller class

--- src/main/java/com/apps/securityapp/controller/AppController.java
+++ src/main/java/com/apps/securityapp/controller/AppController.java
@@ -1,12 +1,26 @@
 package com.apps.securityapp.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.apps.securityapp.dto.SignUpDTO;
+import com.apps.securityapp.model.User;
+import com.apps.securityapp.service.AppService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/auth")
 public class AppController {
+    private final AppService service;
 
+    @Autowired
+    public AppController(AppService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/signup")
+    public ResponseEntity<User> createUser(@Valid @RequestBody SignUpDTO signUpDTO){
+        return service.createUser(signUpDTO);
+    }
 }
