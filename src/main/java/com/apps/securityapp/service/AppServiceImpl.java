f61ada10a8791a5f4aa0328338a34e21552d2bce
williamfisher9 <fisher.a.william@gmail.com>
2023-12-27 04:26:59 -0600

    validated duplicate username and email before saving the record into the database

--- src/main/java/com/apps/securityapp/service/AppServiceImpl.java
+++ src/main/java/com/apps/securityapp/service/AppServiceImpl.java
@@ -2,6 +2,8 @@ package com.apps.securityapp.service;
 
 import com.apps.securityapp.dao.UserRepository;
 import com.apps.securityapp.dto.SignUpDTO;
+import com.apps.securityapp.exception.DuplicateEmailException;
+import com.apps.securityapp.exception.DuplicateUsernameException;
 import com.apps.securityapp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,14 @@ public class AppServiceImpl implements AppService{
 
     @Override
     public ResponseEntity<User> createUser(SignUpDTO dtoUser) {
+        if(userRepository.findByUsername(dtoUser.getUsername()).isPresent()){
+            throw new DuplicateUsernameException(dtoUser.getUsername() + ": user with the same username exists!");
+        }
+
+        if(userRepository.findByEmail(dtoUser.getEmail()).isPresent()){
+            throw new DuplicateEmailException(dtoUser.getEmail() + ": user with the same email address exists!");
+        }
+
         User user = new User();
         user.setEmail(dtoUser.getEmail());
         user.setUsername(dtoUser.getUsername());
